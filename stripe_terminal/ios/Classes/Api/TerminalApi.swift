1d83a0ed14fd098b94e5f618a823410379918b14
loicgeek <loic.ngou98@gmail.com>
2025-10-14 13:29:23 +0100

    fixed nullable onBehalf

--- stripe_terminal/ios/Classes/Api/TerminalApi.swift
+++ stripe_terminal/ios/Classes/Api/TerminalApi.swift
@@ -882,8 +882,8 @@ struct TapToPayConnectionConfigurationApi: ConnectionConfigurationApi {
             autoReconnectOnUnexpectedDisconnect: serialized[0] as! Bool,
             locationId: serialized[1] as! String,
             tosAcceptancePermitted: serialized[2] as! Bool,
-            onBehalfOf: serialized[3] as! String?,
-            merchantDisplayName: serialized[4] as! String?
+            onBehalfOf: serialized[3] as? String,
+            merchantDisplayName: serialized[4] as? String?
         )
     }
 }
